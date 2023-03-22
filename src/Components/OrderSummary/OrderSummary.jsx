5c81bb93973d436d64e16e4492d69c8ab383105a
LieutPaul <Vikas.Kalyanapuram@iiitb.ac.in>
2023-03-22 15:32:51 +0530

    Finished Order Summary

--- src/Components/OrderSummary/OrderSummary.jsx
+++ src/Components/OrderSummary/OrderSummary.jsx
@@ -1,45 +1,46 @@
-import React from 'react'
-import './OrderSummary.scss'
-
+import React from 'react';
+import './OrderSummary.scss';
+import image from './images/illustration-hero.svg';
+import music_icon from "./images/icon-music.svg";
 export default function OrderSummary() {
   return (
-    <div className='root1'>
-        <div class="flex-container-1">
-            <div class="card">
-            <img class="card-img-top" src="./images/illustration-hero.svg" alt="Card image cap" />
-            <div class="mt-4 mb-2">
-                <div class="card-title">Order Summary</div>
+    <div className='order-summary'>
+        <div className="flex-container-1">
+            <div className="card">
+            <img src={image} className="card-img-top" alt="Card cap" />
+            <div className="mt-4 mb-2">
+                <div className="card-title">Order Summary</div>
             </div>
-            <div class="card-text">
+            <div className="card-text">
                 You can now listen to millions of songs, audiobooks, and podcasts on any device anywhere you like!
             </div>
-            <div class="flex justify-content-center">
-                <div class="row plan-div mt-4 flex-container-3">
-                <div class="col-3">
-                    <img src="./images/icon-music.svg" class='img-fluid' alt="" />
+            <div className="flex justify-content-center">
+                <div className="row plan-div mt-4 flex-container-3">
+                <div className="col-3">
+                    <img src={music_icon} className='img-fluid' alt="" />
                 </div>
-                <div class="col-sm-5 col-6 text-left">
+                <div className="col-sm-5 col-6 text-left">
                     Annual Plan <br/> $59.99/year
                 </div>
-                <div class="col-sm-4 col-3 text-right small-text">
+                <div className="col-sm-4 col-3 text-right small-text">
                     Change
                 </div>
                 </div>
             </div>
             
-            <div class="flex-container-2 mt-4">
-                <button class="button">Proceed to Payment</button>
+            <div className="flex-container-2 mt-4">
+                <button className="button">Proceed to Payment</button>
             </div>
-            <div class="mt-3">
-                <a href="#">Cancel Order</a>
+            <div className="mt-3">
+                <a href="/">Cancel Order</a>
             </div>
             </div>
         </div>
 
         
-        <div class="attribution">
-            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-            Coded by <a href="#">Vikas K</a>.
+        <div className="attribution">
+            Challenge by <a href="https://www.frontendmentor.io?ref=challenge" rel="noreferrer" target="_blank">Frontend Mentor</a>. 
+            Coded by <a href="https://github.com/LieutPaul">Vikas K</a>.
         </div>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -3,19 +3,22 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import OrderSummary from './Components/OrderSummary/OrderSummary';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Router>
       <Routes>
+
         <Route path="/" element=
         {
-        <div>
+        <div style={{"padding":"20px"}}>
           <a href="/order-summary">/order-summary</a> : Order Summary Page
         </div>
         } 
         />
         <Route path="/order-summary" element={<OrderSummary/>}/>
+      
       </Routes>
     </Router>
 );
