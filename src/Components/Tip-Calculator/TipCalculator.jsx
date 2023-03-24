5eb2a88dce707f33fe1481bd9ad27a25b9296d27
LieutPaul <Vikas.Kalyanapuram@iiitb.ac.in>
2023-03-24 11:22:00 +0530

    Finished Desktop Design

--- src/Components/Tip-Calculator/TipCalculator.jsx
+++ src/Components/Tip-Calculator/TipCalculator.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import './TipCalculator.scss'
 import logo from './images/logo.svg'
-
+// import dollar from './images/icon-dollar.svg'
+import {BiDollar} from 'react-icons/bi'
 export default function TipCalculator() {
     return (
         <div className='tip-calculator flex flex-column justify-center items-center'>
@@ -23,24 +24,24 @@ export default function TipCalculator() {
                             <div className='tip-amounts mt-2 mb-4'>
                                 <div className='row'>
                                     <div className='col-4'>
-                                        <button>5%</button>
+                                        <button className='tip-amount-button'>5%</button>
                                     </div>
                                     <div className='col-4'>
-                                        <button>10%</button>
+                                        <button className='tip-amount-button'>10%</button>
                                     </div>
                                     <div className='col-4'>
-                                        <button>15%</button>
+                                        <button className='tip-amount-button'>15%</button>
                                     </div>
                                 </div>
                                 <div className='row mt-3'>
                                     <div className='col-4'>
-                                        <button>20%</button>
+                                        <button className='tip-amount-button'>20%</button>
                                     </div>
                                     <div className='col-4'>
-                                        <button>50%</button>
+                                        <button className='tip-amount-button'>50%</button>
                                     </div>
                                     <div className='col-4'>
-                                        <button>Custom</button>
+                                        <button className='tip-amount-button'>Custom</button>
                                     </div>
                                 </div>
                             </div>
@@ -52,11 +53,43 @@ export default function TipCalculator() {
                             </div>
                         </div>
                     </div>
+                    
                     <div className='col-6 h'>
                         <div className='tip-amount'>
 
+                            <div className='row' style={{"color":"white"}}>
+                                <div className='col-4 text-left mt-auto mb-auto'>
+                                    Tip Amount <br></br> <small>/ person</small>
+                                </div>
+                                <div className='col text-right pe-4 final-amount'>
+                                    <div className='flex justify-right items-center'>
+                                        <BiDollar/> 175.67
+                                    </div> 
+                                </div>
+
+                            </div>
+
+                            <div className='row mt-4' style={{"color":"white"}}>
+
+                                <div className='col-4 text-left mt-auto mb-auto'>
+                                    Total <br></br> <small>/ person</small>
+                                </div>
+                                <div className='col text-right pe-4 final-amount'>
+                                    <div className='flex justify-right items-center'>
+                                        <BiDollar/> 175.67
+                                    </div> 
+                                </div>
+
+                            </div>
+
+                            <div className='mt-4 button-row flex justify-center'>
+                                <button className='reset-button'>RESET</button>
+                            </div>
+                            
                         </div>
+                        
                     </div>
+                
                 </div>
             </div>
         </div>
