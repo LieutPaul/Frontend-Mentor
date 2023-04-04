505e6845eca6d58f649d234358e57bac2c021d59
LieutPaul <Vikas.Kalyanapuram@iiitb.ac.in>
2023-04-04 18:44:11 +0530

    Finished Desktop Design

--- src/Components/Intropage-Dropdown/Intropage.jsx
+++ src/Components/Intropage-Dropdown/Intropage.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import './Intropage.scss'
 import logo from './images/logo.svg'
 import desktopHero from './images/image-hero-desktop.png'
+import audiophile from './images/client-audiophile.svg'
+import databiz from './images/client-databiz.svg'
+import maker from './images/client-maker.svg'
+import meet from './images/client-meet.svg'
+import downarrow from './images/icon-arrow-down.svg'
 export default function Intropage() {
     return (
         <div className='intropage overflow-x-hidden'>
@@ -9,19 +14,28 @@ export default function Intropage() {
             <div className='navbar row'>
                 <div className='col-5 flex justify-between items-center'>
                     <img className="logo" src={logo} alt="logo"/>
-                    <div>Features</div>
-                    <div>Company</div>
-                    <div>Careers</div>
-                    <div>About</div>
+                    <div className='navbar-text'>Features &nbsp;<img style={{"float":"right","marginTop":"10px"}} src={downarrow} alt="downarrow"/></div>
+                    <div className='navbar-text'>Company &nbsp;<img style={{"float":"right","marginTop":"10px"}} src={downarrow} alt="downarrow"/></div>
+                    <div className='navbar-text'>Careers</div>
+                    <div className='navbar-text'>About</div>
                 </div>
                 <div className='col-7 flex justify-end items-center'>
-                    <div className='mr-4'>Login</div>
+                    <div className='mr-4 navbar-text'>Login</div>
                     <div className='ml-4'><button className='register-button'>Register</button></div>
                 </div>
             </div>
-            <div className='row hero mt-2 '>
-                <div className="col-6">
 
+            <div className='relative row hero mt-2 ps-4 pe-4'>
+                <div className="col-6 flex flex-column justify-center items-center">
+                    <div className="heading w-5/6">Make remote work</div>
+                    <div className="hero-text w-5/6 mt-4">Get your team in sync, no matter what your location. Streamline processes, create team rituals, and watch productivity soar.</div>
+                    <div className='mt-4 w-5/6'><button className='learnmore-button'>Learn More</button></div>
+                    <div className="clients flex w-5/12 justify-between">
+                        <img src={databiz} className='img-fluid' alt="audiophile"/>
+                        <img src={audiophile} className='img-fluid' alt="audiophile"/>
+                        <img src={meet} className='img-fluid' alt="audiophile"/>
+                        <img src={maker} className='img-fluid' alt="audiophile"/> 
+                    </div>
                 </div>
                 <div className="col-6 flex justify-center items-center">
                     <img src={desktopHero} className="desktop-hero" alt="hero"/>
